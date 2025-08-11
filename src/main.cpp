4f54a5f80ed24ca190f9c0571cc746f5b0bcf275
Michael Cook <entozoon@gmail.com>
2025-08-11 23:06:38 +0100

    moving to pi instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,38 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
-Servo myServo1;
-Servo myServo2;
+#include <math.h>
+
+Servo servo1;
+Servo servo2;
+
+float_t time_i = 0;
+
+const int MIN_ANGLE = 30;
+const int MAX_ANGLE = 150;
+const float_t PHASE_SHIFT = -M_PI / 2;
+
 void setup()
 {
   Serial.begin(115200);
   delay(3000);
-  myServo1.setPeriodHertz(50);
-  myServo2.setPeriodHertz(50);
-  myServo1.attach(2); //, 1000, 2000);
-  myServo2.attach(3); //, 1000, 2000);
+  servo1.setPeriodHertz(50);
+  servo2.setPeriodHertz(50);
+  servo1.attach(2); //, 1000, 2000);
+  servo2.attach(3); //, 1000, 2000);
 }
 void loop()
 {
-  Serial.println("Moving servo to 0 degrees");
-  myServo1.write(0);
-  myServo2.write(0);
-  delay(5000);
-  Serial.println("Moving servo to 90 degrees");
-  myServo1.write(90);
-  myServo2.write(90);
-  delay(5000);
-  Serial.println("Moving servo to 180 degrees");
-  myServo1.write(180);
-  myServo2.write(180);
-  delay(5000);
+  float_t range = MAX_ANGLE - MIN_ANGLE;
+  float_t center = MIN_ANGLE + range / 2;
+  float_t amplitude = range / 2;
+
+  float_t pos1 = center + sin(time_i) * amplitude;
+  float_t pos2 = center + sin(time_i + PHASE_SHIFT) * amplitude;
+
+  servo1.write(pos1);
+  servo2.write(pos2);
+
+  time_i += 0.05;
+
+  delay(10);
 }
